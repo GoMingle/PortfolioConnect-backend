40155a77c4cb1a2895dd8b74480c014a4b36d358
Abigail <asantea331@gmail.com>
2024-07-10 10:03:34 +0000

    created separate model files

---
+++ models/skillM.js
@@ -0,0 +1,12 @@
+import { Schema, model, Types } from "mongoose";
+
+const skillSchema = new Schema(
+    [
+        {
+        name: {type: String},
+        levelOfProficiency: {type: String, enum: ['beginner', 'inter-mediate', 'advanced', 'expert']},
+        user: {type: Types.ObjectId, ref: 'User'}
+    }
+]);
+
+export const Experience = model('Skills', skillSchema);
